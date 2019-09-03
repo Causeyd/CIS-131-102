ca53b3b6e025a37ef9a9f996e0b315d33dde6bf2
Daniel Causey <44783179+Causeyd@users.noreply.github.com>
2019-09-03 12:56:57 -0500

    Add files via upload

---
+++ Functional Image Swapping/scripts.js
@@ -0,0 +1,21 @@
+/*
+function imageChange1(){
+    document.getElementById('WildTalents').src ="Wild Talents.jpg";
+}
+
+function imageChange2(){
+    document.getElementById('WildTalents').src ="Anima.jpg";
+}
+
+function imageChange3(){
+    document.getElementById('WildTalents').src ="Cthulhu.gif";
+}
+
+function imageChange4(){
+     document.getElementById('WildTalents').src ="Pathfinder.jpg";
+}
+*/
+
+function imageChange(currentImage){
+    document.getElementById('WildTalents').src=currentImage;
+}
