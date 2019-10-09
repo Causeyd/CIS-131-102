cc160b0fad16f83ba165b8151e37d971a0efc890
Daniel Causey <44783179+Causeyd@users.noreply.github.com>
2019-10-08 23:59:24 -0500

    Add files via upload

---
+++ The Great Race/scripts.js
@@ -0,0 +1,17 @@
+var raceOn = false;
+var victoryImage = ["redVictory.jpg", "blueVictory.jpg"];
+var stoplightImage = ["stoplight"]
+
+
+//Stoplight Race start
+document.getElementById('Red Stoplight').addEventListener('click',function(){
+  raceOn = true;
+  document.getElementsByClassName('Stoplight')//Set up code to change stoplight from red to green.
+})
+
+//Restart the race
+document.getElementById('victoryImage').addEventListener('click',function(){
+
+})
+
+while(raceOngoing = true;)
