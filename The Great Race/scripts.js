585e3b59c4dd62010487b5a01f84345cfe37b863
Daniel Causey <44783179+Causeyd@users.noreply.github.com>
2019-10-10 09:02:55 -0500

    Add files via upload

--- The Great Race/scripts.js
+++ The Great Race/scripts.js
@@ -1,17 +1,53 @@
 var raceOn = false;
-var victoryImage = ["redVictory.jpg", "blueVictory.jpg"];
-var stoplightImage = ["stoplight"]
+var victoryImage = ["redVictory.png", "blueVictory.png"];
+var stoplightImage = ["stoplightRed.png", "stoplightGreen.png"]
 
 
 //Stoplight Race start
-document.getElementById('Red Stoplight').addEventListener('click',function(){
-  raceOn = true;
-  document.getElementsByClassName('Stoplight')//Set up code to change stoplight from red to green.
+document.getElementById('Stoplight').addEventListener('click', function() {
+//  raceOn = true;
+  document.getElementById('Stoplight').src = stoplightImage[1];
+  startRace();
 })
+
+//Script for the race
+function startRace() {
+  var elem1 = document.getElementById("redCyclist");
+  var pos1 = 20;
+  var elem2 = document.getElementById("blueCyclist");
+  var pos2 = 20;
+  var id = setInterval(frame, 10);
+
+  function frame() {
+    if (pos1 >= 1780 || pos2 >= 1780) {
+      clearInterval(id);
+    } else {
+      pos1 = pos1 + (Math.floor(Math.random() * 11));
+      elem1.style.left = pos1 + 'px';
+      pos2 = pos2 + (Math.floor(Math.random() * 11));
+      elem2.style.left = pos2 + 'px';
+    }
+    if (pos1 >= 1780 && pos2 != 1780){
+      document.getElementById("victoryImage").src = victoryImage[0];
+      document.getElementById("victoryImage").style.display = "block";
+      document.getElementById("victoryImage").addEventListener('click', function(){
+        newRace();
+      });
+    }
+    if (pos1 != 1780 && pos2 >= 1780){
+      document.getElementById("victoryImage").src = victoryImage[1];
+      document.getElementById("victoryImage").style.display = "block";
+      document.getElementById("victoryImage").addEventListener('click', function(){
+        newRace();
+      });
+    }
+  }
+  function newRace(){
+    document.getElementById("blueCyclist").style.left = "20px";
+    document.getElementById("redCyclist").style.left = "20px";
+    document.getElementById('Stoplight').src = stoplightImage[0];
+    document.getElementById('victoryImage').style.display = "none";
+  }
+}
 
 //Restart the race
-document.getElementById('victoryImage').addEventListener('click',function(){
-
-})
-
-while(raceOngoing = true;)
